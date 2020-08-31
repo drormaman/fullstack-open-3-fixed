1bc5e59ddb0edfcb2ef22813c65254f9a48bc264
drormaman <drormaman@gmail.com>
2020-08-31 19:14:27 +0300

    bbb

---
+++ build/precache-manifest.b505f888881d309c00a9f2374da3bc2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deec8e958c37d9721d24b069af52109c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fc978582f94fe95ca32",
+    "url": "/static/css/main.adb0e0ae.chunk.css"
+  },
+  {
+    "revision": "261379cd12f7a257a30b",
+    "url": "/static/js/2.72903e02.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.72903e02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5fc978582f94fe95ca32",
+    "url": "/static/js/main.8dab412f.chunk.js"
+  },
+  {
+    "revision": "8dd7441d529e73d61d47",
+    "url": "/static/js/runtime-main.aaa24d6c.js"
+  }
+]);
